ab2287ce1f281631276ff8b545636be3ed6367b3
Itiua <ValeriyBushenev@gmail.com>
2021-03-15 11:42:40 +0300

    upd

--- veda-camunda-connector/src/common.rs
+++ veda-camunda-connector/src/common.rs
@@ -1,9 +1,6 @@
-use crate::Context;
 use std::error::Error;
 use std::fmt;
 use v_module::module::Module;
-use v_module::v_api::app::generate_unique_uri;
-use v_module::v_api::IndvOp;
 use v_module::v_onto::individual::Individual;
 use v_module::v_onto::onto::Onto;
 
@@ -17,7 +14,7 @@ impl fmt::Display for MyError {
 }
 
 impl Error for MyError {}
-
+/*
 pub fn add_right(subj_uri: &str, obj_uri: &str, ctx: &mut Context, module: &mut Module) -> Result<(), Box<dyn Error>> {
     if subj_uri.is_empty() || obj_uri.is_empty() {
         return Err(Box::new(MyError("empty subj_uri or obj_uri".into())));
@@ -34,7 +31,7 @@ pub fn add_right(subj_uri: &str, obj_uri: &str, ctx: &mut Context, module: &mut 
     module.api.update_or_err(&ctx.sys_ticket, "", "add-right", IndvOp::Put, &right)?;
     Ok(())
 }
-
+*/
 pub(crate) fn is_start_form(rdf_types: &[String], onto: &mut Onto) -> bool {
     for itype in rdf_types {
         if itype == "bpmn:StartForm" {
--- veda-camunda-connector/src/decision_form.rs
+++ veda-camunda-connector/src/decision_form.rs
@@ -1,35 +1,43 @@
 use crate::Context;
-use serde_json::json;
-
 use camunda_client::models::{CompleteTaskDto, VariableValueDto};
 use std::collections::HashMap;
 use std::error::Error;
 use v_module::module::Module;
 use v_module::v_api::IndvOp;
 use v_module::v_onto::individual::Individual;
+use serde_json::json;
 
 pub fn prepare_decision_form(decision_form: &mut Individual, ctx: &mut Context, module: &mut Module, _signal: &str) -> Result<(), Box<dyn Error>> {
-    if let Some(decision) = decision_form.get_first_literal("v-wf:takenDecision") {
+    if let Some(decision_id) = decision_form.get_first_literal("v-wf:takenDecision") {
         if let Some(task_id) = decision_form.get_first_literal("bpmn:taskId") {
             let mut vars = HashMap::new();
             let mut var = VariableValueDto::new();
-            var.value = Some(json!(decision));
-            var._type = Some("json".to_owned());
-            vars.insert("takenDecision".to_owned(), var);
-            let mut params = CompleteTaskDto::new();
-            params.variables = Some(vars);
-            params.with_variables_in_return = Some(true);
+            if let Some(decision) = module.get_individual(&decision_id, &mut Individual::default()) {
+                decision.parse_all();
+                var.value = Some(decision.get_obj().as_json());
+//                var.value = Some(json!(decision_id));
+                var._type = Some("json".to_owned());
+                vars.insert("takenDecision".to_owned(), var);
+                let mut params = CompleteTaskDto::new();
+                params.variables = Some(vars);
+                params.with_variables_in_return = Some(true);
 
-            match ctx.camunda_client.task_api().complete(&task_id, Some(params)) {
-                Ok(_) => {
-                    decision_form.parse_all();
-                    decision_form.set_bool("v-wf:isCompleted", true);
-                    module.api.update_or_err(&ctx.sys_ticket, "", "prepare-decision-process", IndvOp::Put, &decision_form)?;
+                match ctx.camunda_client.task_api().complete(&task_id, Some(params)) {
+                    Ok(_) => {
+                        decision_form.parse_all();
+                        decision_form.set_bool("v-wf:isCompleted", true);
+                        module.api.update_or_err(&ctx.sys_ticket, "", "prepare-decision-process", IndvOp::Put, &decision_form)?;
+                        info!("prepare_decision_form: success send task complete")
+                    }
+                    Err(e) => {
+                        error!("prepare_decision_form: failed to send task complete, err={:?}", e)
+                    }
                 }
-                Err(e) => {
-                    error!("failed to send task complete, err={:?}", e)
-                }
+            } else {
+                error!("prepare_decision_form: not found {}", decision_id);
             }
+        } else {
+            error!("prepare_decision_form: {} not content bpmn:taskId", decision_form.get_id());
         }
     }
 
