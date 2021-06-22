d1b86b25a1f56f92fc8c7b36dc450f20de0554db
Roman Karpov <roman.karpov@gmail.com>
2021-06-22 17:27:23 +0300

    cleanup

--- veda-camunda-plugin/src/main/java/com/semanticmachines/veda/bpm/VedaParseListener.java
+++ veda-camunda-plugin/src/main/java/com/semanticmachines/veda/bpm/VedaParseListener.java
@@ -18,140 +18,143 @@ import org.camunda.bpm.engine.impl.variable.VariableDeclaration;
  */
 public class VedaParseListener extends AbstractBpmnParseListener {
 
+  VedaUserTaskListener taskListener = VedaUserTaskListener.getInstance();
+  VedaExecutionListener executionListener = VedaExecutionListener.getInstance();
+  
   @Override
   public void parseUserTask(Element userTaskElement, ScopeImpl scope, ActivityImpl activity) {
     ActivityBehavior activityBehavior = activity.getActivityBehavior();
     if(activityBehavior instanceof UserTaskActivityBehavior) {
       UserTaskActivityBehavior userTaskActivityBehavior = (UserTaskActivityBehavior) activityBehavior;
       TaskDefinition task = userTaskActivityBehavior.getTaskDefinition();
-      task.addTaskListener("create", VedaUserTaskListener.getInstance());
-      task.addTaskListener("assignment", VedaUserTaskListener.getInstance());
-      task.addTaskListener("update", VedaUserTaskListener.getInstance());
-      task.addTaskListener("delete", VedaUserTaskListener.getInstance());
-      task.addTaskListener("timeout", VedaUserTaskListener.getInstance());
-      task.addTaskListener("complete", VedaUserTaskListener.getInstance());
+      task.addTaskListener("create", taskListener);
+      task.addTaskListener("assignment", taskListener);
+      task.addTaskListener("update", taskListener);
+      task.addTaskListener("delete", taskListener);
+      task.addTaskListener("timeout", taskListener);
+      task.addTaskListener("complete", taskListener);
     }
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseProcess(Element processElement, ProcessDefinitionEntity processDefinition) {
-    processDefinition.addListener("start", VedaExecutionListener.getInstance());
-    processDefinition.addListener("end", VedaExecutionListener.getInstance());
+    processDefinition.addListener("start", executionListener);
+    processDefinition.addListener("end", executionListener);
   }
 
   @Override
   public void parseStartEvent(Element startEventElement, ScopeImpl scope, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseExclusiveGateway(Element exclusiveGwElement, ScopeImpl scope, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
   
   @Override
   public void parseInclusiveGateway(Element inclusiveGwElement, ScopeImpl scope, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseParallelGateway(Element parallelGwElement, ScopeImpl scope, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseScriptTask(Element scriptTaskElement, ScopeImpl scope, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseServiceTask(Element serviceTaskElement, ScopeImpl scope, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseBusinessRuleTask(Element businessRuleTaskElement, ScopeImpl scope, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseTask(Element taskElement, ScopeImpl scope, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseManualTask(Element manualTaskElement, ScopeImpl scope, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseEndEvent(Element endEventElement, ScopeImpl scope, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseBoundaryTimerEventDefinition(Element timerEventDefinition, boolean interrupting, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseBoundaryErrorEventDefinition(Element errorEventDefinition, boolean interrupting, ActivityImpl activity, ActivityImpl nestedErrorEventActivity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseSubProcess(Element subProcessElement, ScopeImpl scope, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseCallActivity(Element callActivityElement, ScopeImpl scope, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseProperty(Element propertyElement, VariableDeclaration variableDeclaration, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseSequenceFlow(Element sequenceFlowElement, ScopeImpl scopeElement, TransitionImpl transition) {
-    transition.addListener("take", VedaExecutionListener.getInstance());
+    transition.addListener("take", executionListener);
   }
 
   @Override
   public void parseSendTask(Element sendTaskElement, ScopeImpl scope, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseMultiInstanceLoopCharacteristics(Element activityElement, Element multiInstanceLoopCharacteristicsElement, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseIntermediateTimerEventDefinition(Element timerEventDefinition, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
@@ -160,92 +163,92 @@ public class VedaParseListener extends AbstractBpmnParseListener {
 
   @Override
   public void parseReceiveTask(Element receiveTaskElement, ScopeImpl scope, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseIntermediateSignalCatchEventDefinition(Element signalEventDefinition, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseBoundarySignalEventDefinition(Element signalEventDefinition, boolean interrupting, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseEventBasedGateway(Element eventBasedGwElement, ScopeImpl scope, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseTransaction(Element transactionElement, ScopeImpl scope, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseCompensateEventDefinition(Element compensateEventDefinition, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseIntermediateThrowEvent(Element intermediateEventElement, ScopeImpl scope, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseIntermediateCatchEvent(Element intermediateEventElement, ScopeImpl scope, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseBoundaryEvent(Element boundaryEventElement, ScopeImpl scopeElement, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseIntermediateMessageCatchEventDefinition(Element messageEventDefinition, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseBoundaryMessageEventDefinition(Element element, boolean interrupting, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseBoundaryEscalationEventDefinition(Element escalationEventDefinition, boolean interrupting, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseBoundaryConditionalEventDefinition(Element element, boolean interrupting, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseIntermediateConditionalEventDefinition(Element conditionalEventDefinition, ActivityImpl activity) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }
 
   @Override
   public void parseConditionalStartEventForEventSubprocess(Element element, ActivityImpl activity, boolean interrupting) {
-    activity.addListener("start", VedaExecutionListener.getInstance());
-    activity.addListener("end", VedaExecutionListener.getInstance());
+    activity.addListener("start", executionListener);
+    activity.addListener("end", executionListener);
   }  
   
 }
